b761bcbfbcb22a5a9e565dd4423efd54d3f7ad15
Lil-Kolton <Lil-Kolton@users.noreply.github.com>
2026-06-14 13:43:11 +0800

    feat(Micro_E53_SmartLamp): 智慧台灯功能完善
    - 修复 config.json: apiVersion 7/Beta2, icon/label 引用格式
    - 修复 BUILD.gn: assets_path 改为 entry/src/main 确保 HAP 路径正确
    - 新增 ludeng.js: 亮度±调节、场景模式(阅读/放松/夜间)、定时关灯(±5分钟)、开/关灯
    - 新增 ludeng.hml: 完整页面布局(亮度区/场景区/定时区/开关区)
    - 新增 ludeng.css: 完整样式(flex布局/按钮/颜色)
    - 打包编译版 HAP: 基于 webpack 格式，兼容 OpenHarmony Lite JS 框架
    - 关灯保留亮度记忆，开灯恢复上次亮度

--- bearpi-hm_micro_small/applications/BearPi/BearPi-HM_Micro/samples/Micro_E53_SmartLamp/entry/src/main/js/default/pages/ludeng/ludeng.js
+++ bearpi-hm_micro_small/applications/BearPi/BearPi-HM_Micro/samples/Micro_E53_SmartLamp/entry/src/main/js/default/pages/ludeng/ludeng.js
@@ -1,7 +1,19 @@
 import router from '@system.router';
 import app from '@system.app';
 
-var cmdno = {init:0, close:1, read:2, deng:3, brightness:4};
+var cmd = {
+    init: 0,
+    close: 1,
+    read: 2,
+    deng: 3,
+    brightness: 4
+};
+
+var sceneBrightness = {
+    read: 100,
+    relax: 60,
+    night: 10
+};
 
 export default {
     data: {
@@ -10,55 +22,226 @@ export default {
             LED: 'OFF',
             Brightness: 0
         },
-        interval: 0
+        interval: 0,
+        timerInterval: 0,
+        currentScene: 'custom',
+        customBrightness: 50,
+        selectedTimer: 0,
+        timerRemaining: 0,
+        timerDisplay: ''
     },
 
     onInit() {
         app.e53sc1service({
-            cmd: cmdno.init,
-            success(res){},
-            fail(res){},
-            complete(res){}
+            cmd: cmd.init,
+            success(res) {},
+            fail(res) {},
+            complete(res) {}
         });
+        this.queryData();
         this.interval = setInterval(() => this.queryData(), 2000);
     },
 
     onDestroy() {
         if (this.interval) {
             clearInterval(this.interval);
+            this.interval = 0;
+        }
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+            this.timerInterval = 0;
         }
         app.e53sc1service({
-            cmd: cmdno.close,
-            success(res){},
-            fail(res){},
-            complete(res){}
+            cmd: cmd.close,
+            success(res) {},
+            fail(res) {},
+            complete(res) {}
         });
+    },
+
+    toback() {
+        this.onDestroy();
+        router.replace({
+            uri: 'pages/index/index'
+        });
+    },
+
+    exit() {
+        this.toback();
+    },
+
+    applyState(data) {
+        this.mydata = data;
+        var brightness = Number(data.Brightness);
+        if (isNaN(brightness)) {
+            brightness = 0;
+        }
+        if (data.LED !== 'ON') {
+            this.currentScene = 'custom';
+            this.customBrightness = brightness;
+            return;
+        }
+        if (brightness === sceneBrightness.read) {
+            this.currentScene = 'read';
+        } else if (brightness === sceneBrightness.relax) {
+            this.currentScene = 'relax';
+        } else if (brightness === sceneBrightness.night) {
+            this.currentScene = 'night';
+        } else {
+            this.currentScene = 'custom';
+            this.customBrightness = brightness;
+        }
+    },
+
+    handleServiceResponse(res) {
+        if (!res || !res.e53_sc1) {
+            return;
+        }
+        var data = JSON.parse(res.e53_sc1);
+        this.applyState(data);
+    },
+
+    sendPower(state) {
+        var that = this;
+        app.e53sc1service({
+            cmd: cmd.deng,
+            data: state,
+            success(res) {
+                that.handleServiceResponse(res);
+            },
+            fail(res, code) {},
+            complete() {}
+        });
+    },
+
+    sendBrightness(value) {
+        var that = this;
+        app.e53sc1service({
+            cmd: cmd.brightness,
+            data: String(value),
+            success(res) {
+                that.handleServiceResponse(res);
+            },
+            fail(res, code) {},
+            complete() {}
+        });
+    },
+
+    turnOn() {
+        this.sendPower('ON');
+    },
+
+    turnOff() {
+        this.sendPower('OFF');
     },
 
     toggleLight() {
-        let that = this;
-        let cmd = (this.mydata.LED === 'ON') ? 'OFF' : 'ON';
-        app.e53sc1service({
-            cmd: cmdno.deng,
-            data: cmd,
-            success(res) {
-                that.mydata = JSON.parse(res.e53_sc1);
-            },
-            fail(res){},
-            complete(res){}
-        });
+        this.sendPower(this.mydata.LED === 'ON' ? 'OFF' : 'ON');
     },
 
     queryData() {
-        let that = this;
+        var that = this;
         app.e53sc1service({
-            cmd: cmdno.read,
+            cmd: cmd.read,
             data: '',
             success(res) {
-                that.mydata = JSON.parse(res.e53_sc1);
+                that.handleServiceResponse(res);
             },
-            fail(res){},
-            complete(res){}
+            fail(res, code) {},
+            complete() {}
         });
+    },
+
+    onBrightnessChange(e) {
+        var brightness = Number(e.value);
+        if (isNaN(brightness)) {
+            brightness = 0;
+        }
+        this.currentScene = 'custom';
+        this.customBrightness = brightness;
+        this.sendBrightness(brightness);
+    },
+
+    setScene(scene) {
+        var brightness = sceneBrightness[scene];
+        if (scene === 'custom') {
+            brightness = this.customBrightness;
+        } else {
+            this.customBrightness = Number(this.mydata.Brightness);
+        }
+        this.currentScene = scene;
+        this.sendBrightness(brightness);
+    },
+
+    sceneRead() {
+        this.setScene('read');
+    },
+
+    sceneRelax() {
+        this.setScene('relax');
+    },
+
+    sceneNight() {
+        this.setScene('night');
+    },
+
+    sceneCustom() {
+        this.setScene('custom');
+    },
+
+    setTimer(minutes) {
+        var that = this;
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+            this.timerInterval = 0;
+        }
+        if (minutes <= 0) {
+            this.cancelTimer();
+            return;
+        }
+        this.selectedTimer = minutes;
+        this.timerRemaining = minutes * 60;
+        this.updateTimerDisplay();
+        this.timerInterval = setInterval(() => {
+            that.timerRemaining--;
+            if (that.timerRemaining <= 0) {
+                that.autoOff();
+                that.cancelTimer();
+                return;
+            }
+            that.updateTimerDisplay();
+        }, 1000);
+    },
+
+    timer10() {
+        this.setTimer(10);
+    },
+
+    timer30() {
+        this.setTimer(30);
+    },
+
+    timer60() {
+        this.setTimer(60);
+    },
+
+    cancelTimer() {
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+            this.timerInterval = 0;
+        }
+        this.selectedTimer = 0;
+        this.timerRemaining = 0;
+        this.timerDisplay = '';
+    },
+
+    updateTimerDisplay() {
+        var min = Math.floor(this.timerRemaining / 60);
+        var sec = this.timerRemaining % 60;
+        this.timerDisplay = (min < 10 ? '0' : '') + min + ':' + (sec < 10 ? '0' : '') + sec;
+    },
+
+    autoOff() {
+        this.sendPower('OFF');
     }
 }
